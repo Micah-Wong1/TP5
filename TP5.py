d4d888284a5ef3c414bd8f2251be71c740c89937
Micah-Wong1 <Wongm@educ.college-montreal.qc.ca>
2026-02-11 10:44:21 -0500

    hello

--- TP5.py
+++ TP5.py
@@ -1,1 +1,27 @@
-import Arcade
+import arcade
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+WINDOW_TITLE = "Arcade?"
+
+
+class MyGame(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+
+
+def main():
+    arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE)
+    arcade.set_background_color(arcade.color.PALE_BLUE)
+
+    arcade.start_render()
+    arcade.finish_render()
+    arcade.run()
+
+
+def on_draw():
+    arcade.draw_circle_filled(10, 10, 20, (255, 54, 34))
+
+
+main()
+on_draw()
